8267a4e47cd66ca8ec2107fdd378aefc2e05c7b5
TDumler74 <tatjana.dumler@gmail.com>
2023-05-23 16:32:59 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) throws ParseException {
+
+    List<Person> person = new ArrayList<>();
+    person.add(new Person("John", "22.05.1980"));
+    person.add(new Person("Marie", "01.10.2002"));
+    person.add(new Person("Marie", "01.01.2006"));
+    person.add(new Person("Marie", "01.02.2005"));
+
+    System.out.println(person);
+    Collections.sort(person);
+    System.out.println(person);
+
+
+  }
+}
---
+++ src/Person.java
@@ -0,0 +1,49 @@
+
+//Создайте класс Person, которому при создании передавайте имя и
+// дату рождения в виде строки в формате "ДД.ММ.ГГГГ".
+//
+//Реализуйте в классе интерфейс Comparable<Person>, в котором
+// "меньше" будет значить "младше".
+//
+//Подсказка: не обязательно вычислять возраст, этому мы так и не научились.
+//Но можно сравнивать даты рождения!
+//
+//Альтернативная подсказка: если очень хочется вычислять возраст,
+// то можно это делать в миллисекундах с использованием getTime()!
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Person implements Comparable<Person>{
+  private String name;
+  private Date birthday;
+  SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+
+  public Person(String name,String birthday)throws ParseException {
+    this.name=name;
+    this.birthday=format.parse(birthday);
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Date getBirthday() {
+    return birthday;
+  }
+
+  @Override
+  public String toString() {
+    return  name +" " + format.format(birthday);
+  }
+
+  @Override
+  public int compareTo(Person o) {
+
+    return -birthday.compareTo(o.birthday);
+  }
+
+}
